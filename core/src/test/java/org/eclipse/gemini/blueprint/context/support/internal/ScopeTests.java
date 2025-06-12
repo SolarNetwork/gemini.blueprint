55499372444f5698940d7e6a1795830e7e5afe6e
Matt Magoffin <matt@solarnetwork.net>
2025-06-12 16:47:35 +1200

    Refactor from removed XmlBeanFactory.

--- core/src/test/java/org/eclipse/gemini/blueprint/context/support/internal/ScopeTests.java
+++ core/src/test/java/org/eclipse/gemini/blueprint/context/support/internal/ScopeTests.java
@@ -23,7 +23,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.Scope;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.util.ClassUtils;
@@ -79,14 +79,18 @@ public class ScopeTests extends TestCase {
 	private DefaultListableBeanFactory bf;
 
 
-	private class ScopedXmlFactory extends XmlBeanFactory {
+	private class ScopedXmlFactory extends DefaultListableBeanFactory {
+
+		private final XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(this);
 
 		public ScopedXmlFactory(Resource resource, BeanFactory parentBeanFactory) throws BeansException {
-			super(resource, parentBeanFactory);
+			super(parentBeanFactory);
+			this.reader.loadBeanDefinitions(resource);
 		}
 
 		public ScopedXmlFactory(Resource resource) throws BeansException {
-			super(resource);
+			super();
+			this.reader.loadBeanDefinitions(resource);
 			registerScope("foo", new FooScope());
 			registerScope("bar", new FooScope());
 		}
