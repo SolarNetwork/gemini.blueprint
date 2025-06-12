b36162bb3727b1b67bcc5b9dfe27bf2cefa0bb62
Matt Magoffin <matt@solarnetwork.net>
2025-06-12 16:47:05 +1200

    Add missing destroy() method from DisposableBean.

--- core/src/main/java/org/eclipse/gemini/blueprint/context/support/OsgiBundleXmlApplicationContext.java
+++ core/src/main/java/org/eclipse/gemini/blueprint/context/support/OsgiBundleXmlApplicationContext.java
@@ -268,4 +268,8 @@ public class OsgiBundleXmlApplicationContext extends AbstractDelegatedExecutionA
 	public String[] getConfigLocations() {
 		return super.getConfigLocations();
 	}
+
+	public void destroy() throws Exception {
+		// extending classes do something
+	}
 }
